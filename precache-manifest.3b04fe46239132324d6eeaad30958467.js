241cc88ec01fd7c24c8ec38f13b96276def34fee
Sroramensah6 <mensahsrora36@gmail.com>
2020-05-15 12:19:26 -0700

    first commit

---
+++ precache-manifest.3b04fe46239132324d6eeaad30958467.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca7be97ca5fb7b130bb7665c3715155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03fe31c39f2059c98219",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "dbb88341d4e21f1ba98b",
+    "url": "/static/css/main.343c2f2f.chunk.css"
+  },
+  {
+    "revision": "03fe31c39f2059c98219",
+    "url": "/static/js/2.5a3ad190.chunk.js"
+  },
+  {
+    "revision": "933c9bab249556188bb68aec139ba8cb",
+    "url": "/static/js/2.5a3ad190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb88341d4e21f1ba98b",
+    "url": "/static/js/main.dbf48d83.chunk.js"
+  },
+  {
+    "revision": "f080d5d8a7d4a6657295",
+    "url": "/static/js/runtime-main.95b37c48.js"
+  },
+  {
+    "revision": "0fcb5f5a8815f8a9c4646b77c5d8078c",
+    "url": "/static/media/20190319_093102.0fcb5f5a.jpg"
+  },
+  {
+    "revision": "02c5e0500f82da113ec0b0316ba88b44",
+    "url": "/static/media/Original-Hero.02c5e050.jpg"
+  },
+  {
+    "revision": "1a4303fc514f67c1003cab0fbf50700a",
+    "url": "/static/media/VPA.1a4303fc.png"
+  },
+  {
+    "revision": "cc229f81f5f5a11f2a417c6e621744f2",
+    "url": "/static/media/hardco.cc229f81.jpg"
+  },
+  {
+    "revision": "33304501e39784517e5875338c6514a3",
+    "url": "/static/media/img11.33304501.jpg"
+  },
+  {
+    "revision": "e933d8674979167dc1baf5d5311183e1",
+    "url": "/static/media/img12.e933d867.jpg"
+  },
+  {
+    "revision": "d168d363ab9481374cd09aa0e008682e",
+    "url": "/static/media/img13.d168d363.jpg"
+  },
+  {
+    "revision": "a1950230360f7d883a112fb39a3c69e4",
+    "url": "/static/media/logos.a1950230.png"
+  },
+  {
+    "revision": "15a46f179afdbdc5af6454a8c6f28098",
+    "url": "/static/media/logotrans.15a46f17.png"
+  },
+  {
+    "revision": "29c9c11544ccc1c5e581b39ee26a3235",
+    "url": "/static/media/luxepar.29c9c115.jpg"
+  }
+]);
